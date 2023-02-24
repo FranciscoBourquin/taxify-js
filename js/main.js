aca3abdf2bd337bdc047d922323365a7ed609839
francisco bourquin <fran.bourquin89@gmail.com>
2023-02-24 16:06:45 -0300

    Reestructuración del código. Se valida la edad con un if. Si el usuario es menor de 15 años no puede usar el servicio. Si es mayor se piden calle y altura con diferentes validaciones

--- js/main.js
+++ js/main.js
@@ -1,31 +1,55 @@
+// creación de variables
 alert ("Bienvenido a Taxify, tu forma segura de viajar");
-let altura;
-let calle;
 
-function pedir_taxi (calle, altura) {
-    calle = prompt ("Por favor, ingresa la calle");
-    let long= calle.length;
+let calle = "";
+let altura = 0;
 
-    while (long===0) {
-        calle = prompt("La calle ingresada es incorrecta, intente de nuevo.");
-        long = calle.length;
-    };
+// Creo la función
 
-    if (long > 0) {
-        altura = parseInt(prompt("Ingrese la altura"));
-    };
+function pedir_taxi(calle, altura) {
+    edad = 0; 
 
-    while (altura<4 || altura>9999 || isNaN(altura)) {
-        altura = parseInt(prompt("La altura ingresada es incorrecta"));
+    //Pedimos la edad
+
+    edad = parseInt(prompt("Por favor ingresá tu edad"));
+
+    if (edad < 15) {
+        alert("Por cuestiones de seguridad no permitimos que menores de 15 años utilicen la aplicación");
+        alert("Gracias por confiar en Taxify.")
+
+    } else {
+
+            // Pedimos la calle 
+
+    calle = prompt ("Por favor ingresá la calle");
+    long = calle.length
+
+    //Validamos que sea una calle de longitud válida
+
+    while (long<4) {
         
-    };
+        alert ("La calle ingresada es incorrecta. Intentá de nuevo");
+        calle = prompt("Por favor ingresá la calle"); 
+        long = calle.length
+    }
 
-    if (altura>0 || altura <= 9999) {
-        alert ("Buscando el móvil más cercano");
-        alert ("Tu taxi está en camino. Que tengas un buen viaje!")
-    };
+//Pedimos la altura y convertimos a número
 
-    };
+altura = parseInt(prompt("Por favor ingresa la altura"));
 
+while (altura>9999 || altura<0 || isNaN(altura)) {
+    
+    alert("Altura incorrecta por favor, intentá de nuevo");
+    altura = prompt("Ingresá la altura");
+}
 
-pedir_taxi(calle, altura);
+alert("Buscando el móvil más cercano"); 
+alert("Tu taxi está en camino. Que tengas buen viaje!");
+alert("Gracias por confiar en Taxify.");
+
+}
+    }
+    
+// Llamo la función
+
+pedir_taxi (calle, altura);
