b64c27f41f52578cc59c230b9d8211bf3f4b4937
francisco bourquin <fran.bourquin89@gmail.com>
2023-02-23 22:30:29 -0300

    Proyecto taxify js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+alert ("Bienvenido a Taxify, tu forma segura de viajar");
+let altura;
+let calle;
+
+function pedir_taxi (calle, altura) {
+    calle = prompt ("Por favor, ingresa la calle");
+    }
+
+
+pedir_taxi(calle, altura);
