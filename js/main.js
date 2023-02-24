f1454485be8b8d609c1fa23914a9b5c449af8470
francisco bourquin <fran.bourquin89@gmail.com>
2023-02-24 00:37:39 -0300

    valido altura de la calle. Si es incorrecta entra en while hasta ingresar algo válido

--- js/main.js
+++ js/main.js
@@ -4,7 +4,28 @@ let calle;
 
 function pedir_taxi (calle, altura) {
     calle = prompt ("Por favor, ingresa la calle");
-    }
+    let long= calle.length;
+
+    while (long===0) {
+        calle = prompt("La calle ingresada es incorrecta, intente de nuevo.");
+        long = calle.length;
+    };
+
+    if (long > 0) {
+        altura = parseInt(prompt("Ingrese la altura"));
+    };
+
+    while (altura<4 || altura>9999 || Number.isNaN(altura)) {
+        altura = parseInt(prompt("La altura ingresada es incorrecta"));
+        
+    };
+
+    if (altura>0 || altura <= 9999) {
+        alert ("Buscando el móvil más cercano");
+        alert ("Tu taxi está en camino. Que tengas un buen viaje!")
+    };
+
+    };
 
 
 pedir_taxi(calle, altura);
