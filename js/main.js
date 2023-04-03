d95bebaaeffc09aeaf74732c28256ba5dc58ee90
francisco bourquin <fran.bourquin89@gmail.com>
2023-04-03 12:02:07 -0300

    Se modifican las funciones para hacerlas más generales

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-//Creamos variables que traen info del DOM
-let calle1 = document.getElementById("calle_origen"),
+//Creamos constantes que traen info del DOM
+const calle1 = document.getElementById("calle_origen"),
 error1 = document.getElementById("error1"),
 altura1 = document.getElementById("altura_origen"),
 error2 = document.getElementById("error2"),
@@ -13,62 +13,34 @@ choferes = document.getElementById("choferes");
 //Declaramos las funciones que responderán a los eventos 
 
 //La función valida ambas calles
-function validar_calle (p) {
-    if (p === calle1) { //Si el parámetro es calle1
-            if (calle1.value.length<4 || parseInt(calle1.value)) {
-                error1.innerHTML+= '<br> <p class="inline-block bg-red-600 text-white font-bold">La calle ingresada es incorrecta</p>'
-                calle1.addEventListener("focus", ()=> {
-                error1.innerHTML = "";
-                    })
-            
-           } else {
-                    error1.innerHTML+= '<i class="fa-solid fa-circle-check fa-xl" style="color: #55ca58;"></i>'
-                }
+function validar_calle (calle, error) {
+    if (parseInt(calle)<10|| isNaN(calle)) {
+        error.innerHTML= '<br> <p class= "inline-block bg-red-600 text-white font-bold">La calle ingresada es incorrecta</p>' 
+        calle.addEventListener("focus", ()=> {
+            error.innerHTML= "";
+        })
+    } else {
         
-        } 
-        else {
-            if (calle2.value.length<4 || parseInt(calle2.value)) {
-                error3.innerHTML+= '<br> <p class="inline-block bg-red-600 text-white font-bold">La calle ingresada es incorrecta</p>'
-                calle2.addEventListener("focus", ()=> {
-                error3.innerHTML = "";
-                    })
-            
-           } else {
-                    error3.innerHTML+= '<i class="fa-solid fa-circle-check fa-xl" style="color: #55ca58;"></i>'
-                }
-        }
-
+    }
 }
 
-function validar_altura (p) {
-    if (p === altura1) { //Si el parámetro es altura1
-        if (isNaN(altura1.value)  || altura1.value<10) {
-            error2.innerHTML+= '<br> <p class="inline-block bg-red-600 text-white font-bold">La altura ingresada es incorrecta</p>'
-            altura1.addEventListener("focus", ()=> {
-            error2.innerHTML = "";
-            })
-        
-       } else {
-                error2.innerHTML+= '<i class="fa-solid fa-circle-check fa-xl" style="color: #55ca58;"></i>'
-            }
-    
-    } 
+
+function validar_altura (altura, error) {
+    if (parseInt(altura)<10|| isNaN(altura)) {
+        error.innerHTML= '<br> <p class= "inline-block bg-red-600 text-white font-bold">La altura ingresada es incorrecta</p>';
+        error.innerHTML= '<br> <p class= "inline-block bg-red-600 text-white font-bold">La calle ingresada es incorrecta</p>' 
+        altura.addEventListener("focus", ()=> {
+        error.innerHTML= "";
+    })} 
     else {
-        if (isNaN(altura2.value)  || altura2.value<10) {
-            error4.innerHTML+= '<br> <p class="inline-block bg-red-600 text-white font-bold">La altura ingresada es incorrecta</p>';
-            altura2.addEventListener("focus", ()=> {
-            error4.innerHTML = "";
-                })
-        
-       } else {
-                error4.innerHTML+= '<i class="fa-solid fa-circle-check fa-xl" style="color: #55ca58;"></i>'
-            }
+        error.innerHTML+= '<i class="fa-solid fa-circle-check fa-xl" style="color: #55ca58;"></i>'
     }
+    
 };
 
 function asignar_chofer () {
     //Se crea una clase que servirá de molde para los objetos que se creen posteriormente 
-const chofer = class chofer {
+let chofer= class chofer {
     constructor (apellido, nombre, edad, DNI) {
         this.apellido = apellido;
         this.nombre = nombre;
@@ -92,38 +64,38 @@ while (num>2) {
     };
 
     alert("El chofer asignado a tu viaje es " + JSON.stringify(conductores[num]));
-    alert("Por cuestiones de seguridad pedile al chofer que te muestre alguna identificación que valide su identidad antes de ingresar al móvil");
+    alert("Por cuestiones de seguridad pedile al chofer que te muestre alguna identificación que valide su identidad antes de ingresar al móvil");}
 
-}
+
 
 
 //creamos los eventos 
 calle1.addEventListener("change", ()=> {
-    validar_calle(calle1)
+    validar_calle(calle1, error1)
 });
 
 calle2.addEventListener("change", ()=> {
-    validar_calle(calle2)
+    validar_calle(calle2,error3)
 });
 
 altura1.addEventListener("change", ()=> {
-    validar_altura(altura1)
+    validar_altura(altura1, error2)
 });
 
 altura2.addEventListener("change", ()=> {
-    validar_altura(altura2)
+    validar_altura(altura2, error4)
 });
 
 btn.addEventListener("click", (evento)=>{
     if (calle1.value === ""|| calle2.value ===""|| altura1.value ===""|| altura2.value === "") {
         evento.preventDefault();
 
-        choferes.innerHTML = '<br> <p class= "inline-block bg-red-600 text-white font-bold">Debes completar todos los campos para poder pedir tu taxi</p>'
-       btn.addEventListener("blur", ()=> {
-        choferes.innerHTML= "";
-       })
+        choferes.innerHTML = '<br> <p class= "inline-block bg-red-600 text-white font-bold">Debes completar todos los campos para poder pedir tu taxi</p>';
+        btn.addEventListener("blur", ()=> {
+            choferes.innerHTML = "";
+        })
     } else {
         asignar_chofer();
         
     }
-});
+})
